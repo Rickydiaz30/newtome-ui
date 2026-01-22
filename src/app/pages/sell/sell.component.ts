0f4dd3690e5fcc09d94d492e03772a3b1a515913
Ricky Diaz <diazricky28@gmail.com>
2026-01-22 16:17:31 -0500

    added routerlink to sell button

--- src/app/pages/sell/sell.component.ts
+++ src/app/pages/sell/sell.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sell',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './sell.component.html',
-  styleUrls: ['./sell.component.css']
+  styleUrls: ['./sell.component.css'],
 })
 export class SellComponent {
-
+  gotClicked() {
+    console.log('I got clicked');
+  }
 }
