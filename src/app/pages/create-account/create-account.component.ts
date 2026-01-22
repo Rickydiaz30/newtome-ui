51646b7de2fd0fe8f57765dc1aff3796eadc31d4
Ricky Diaz <diazricky28@gmail.com>
2026-01-22 15:29:53 -0500

    added create an account form

--- src/app/pages/create-account/create-account.component.ts
+++ src/app/pages/create-account/create-account.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './create-account.component.html',
-  styleUrls: ['./create-account.component.css']
+  styleUrls: ['./create-account.component.css'],
 })
 export class CreateAccountComponent {
-
+  gotClicked() {
+    console.log('I got clicked');
+  }
 }
