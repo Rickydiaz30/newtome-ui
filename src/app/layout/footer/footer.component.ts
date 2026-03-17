941c050a18f58575fccd2bede289ac229ac068cb
Ricky <diazricky28@gmail.com>
2026-03-17 07:42:42 -0400

    made sell link active when logged in and create account link active when logged out in footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -11,6 +12,8 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  constructor(public authService: AuthService) {}
+
   currentYear = new Date().getFullYear();
 
   gotClicked() {
